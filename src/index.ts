a10f731ace64971e01c7a419e5a97f5b2bac4951
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2022-01-19 23:07:01 +0200

    feat: index import

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Core
+export { GoogleSaIdToken } from './google-sa-id-token';
+
+// Types
+export { TokenCache, TokenPayload, TokenRaw } from './token.types';
+
+// Helpers
+export { generateExampleSaToken } from './utils/generate-example-sa-token';
